0037e599b127a1e675ea24d2674d85ee76285dd3
Igor-Kostyrenko <xevis84@gmail.com>
2023-07-01 00:09:34 +0300

    edit calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,25 @@
-line 1
+# calc function
+
+def calculate(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        if num2 != 0:  
+            return num1 / num2
+        else:
+            return "error: division by zero!"
+    else:
+        return "Invalid operation"
+
+
+num1 = int(input("input first number: "))
+num2 = int(input("input second number: "))
+operator = input("input operator (+, -, *, /): ")
+
+
+result = calculate(num1, num2, operator)
+print("Result:", result)
